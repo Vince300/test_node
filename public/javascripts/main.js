2b443ede5f7e67ca12afda1fc071a09831caee01
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-03-21 22:06:33 +0100

    Commit initial

---
+++ public/javascripts/main.js
@@ -0,0 +1,10 @@
+﻿jQuery(function($) {
+    $('label').each(function () {
+        var id = $(this).attr('for');
+        var $input = $('#' + id);
+        if ($input.is('select') || $input.is('input') && $input.attr('type') == 'text') {
+            $(this).hide();
+            $input.attr('placeholder', $(this).text());
+        }
+    });
+})(jQuery);
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+﻿var express = require('express');
+var router = express.Router();
+
+// List of options for the register form fields
+var form_options = {
+    age_ranges: ['18-25', '25-35', '35-60', '60+'],
+    meals: [
+        {
+            id: 'breakfast',
+            name: 'Petit-déjeuner',
+            options: ['Croissants', 'Pains au chocolat']
+        },
+        {
+            id: 'breakfast',
+            name: 'Déjeuner',
+            options: ['Viande', 'Poisson']
+        },
+        {
+            id: 'supper',
+            name: 'Dîner',
+            options: ['Fondue', 'Raclette']
+        }
+    ]
+};
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    // Page-specific options
+    var options = { title: 'Test Node.js' };
+    // Extend with the form data
+    for (var k in form_options) options[k] = form_options[k];
+    // Render the correct template
+    res.render('index', options);
+});
+
+/* POST register. */
+router.post('/register', function (req, res) {
+    console.log(req.body);
+    res.send('not yet implemented');
+});
+
+module.exports = router;
