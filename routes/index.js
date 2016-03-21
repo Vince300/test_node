5e2eb6cb3b8b4a9dff0aab9e3d88e652f0c165ef
Vincent Tavernier <vincent.tavernier@ensimag.grenoble-inp.fr>
2016-03-21 23:54:30 +0100

    Ajout traitant inscription -> fichier CSV

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 ﻿var express = require('express');
 var router = express.Router();
+var csv = require('fast-csv');
+var validator = require('validator');
 
 // List of options for the register form fields
 var form_options = {
@@ -11,7 +13,7 @@ var form_options = {
             options: ['Croissants', 'Pains au chocolat']
         },
         {
-            id: 'breakfast',
+            id: 'lunch',
             name: 'Déjeuner',
             options: ['Viande', 'Poisson']
         },
@@ -22,6 +24,31 @@ var form_options = {
         }
     ]
 };
+
+// Form validation data structure, based on form_options
+// This is a list of fields that must pass some validation function, and that are required
+//  - element 0 is an error message
+//  - element 1 is a sanitizer function
+//  - element 2 is a validation function that returns true if the value is valid
+var form_validators = {
+    'first-name': ['Vous devez entrer un prénom.', function (x) { return validator.trim(x); }, function (x) { return x !== null && x !== ""; }],
+    'last-name': ['Vous devez entrer un nom.', function (x) { return validator.trim(x); }, function (x) { return x !== null && x !== ""; }],
+    'age': ['Votre age doit être dans la liste de tranches d\'age.', function (x) { return x; }, function (x) { return validator.isIn(x, form_options.age_ranges); }]
+};
+
+// Build validation parameters for the meals
+for (var i in form_options.meals) {
+    var m = form_options.meals[i];
+    
+    // Build the validator
+    form_validators['meal-' + m.id] = [
+        'Vous devez spécifier votre ' + m.name.toLowerCase() + ' préféré.',
+        parseInt,
+        function (x) {
+            return x !== NaN && x >= 0 && x < m.options.length;
+        }
+    ];
+}
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -35,8 +62,60 @@ router.get('/', function (req, res) {
 
 /* POST register. */
 router.post('/register', function (req, res) {
-    console.log(req.body);
-    res.send('not yet implemented');
+    // The list of error messages
+    var err = [];
+    
+    // Execute the validation process described above
+    for (var k in form_validators) {
+        if (req.body[k] === null) {
+            console.log("Value " + k + " missing");
+            err.push(form_validators[k][0]);
+        } else {
+            req.body[k] = form_validators[k][1](req.body[k]);
+            console.log("Sanitized value " + k + " to '" + req.body[k] + "'");
+            if (!form_validators[k][2](req.body[k])) {
+                console.log("It then did not pass validation");
+                err.push(form_validators[k][0]);
+            } else {
+                console.log("It then passed validation");
+            }
+        }
+    }
+    
+    // Find correct delimiter
+    var delimiter = ',';
+    if (req.body['mode'] === 'excel') {
+        delimiter = ';';
+    } else if (req.body['mode'] !== 'normal') {
+        err.push("Mode " + req.body['mode'] + " inconnu");
+    }
+    
+    // Write CSV using form parameters
+    if (err.length == 0) {
+        res.setHeader('content-type', 'text/csv');
+        res.setHeader('content-disposition', 'attachment; filename="inscription.csv"');
+        
+        // Fixed data
+        var data = [
+            ['Nom complet', req.body['first-name'] + ' ' + req.body['last-name'].toUpperCase()],
+            ['Age', req.body['age']]];
+        
+        // Append prefered meal data
+        for (var m in form_options.meals) {
+            // The actual item m
+            m = form_options.meals[m];
+            // Find the id of the corresponding input
+            var id = 'meal-' + m.id;
+            // Get the full string for this value
+            data.push([m.name, m.options[req.body[id]]]);
+        }
+        
+        // Write output CSV
+        csv.write(data, { headers: true, delimiter: delimiter }).pipe(res);
+    } else {
+        // Render error page
+        res.render('register_error', { title: 'Test Node.js', errors: err });
+    }
 });
 
 module.exports = router;
